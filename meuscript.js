790430818862972db9356c656add092a4adc4835
rnasciment0 <rivan.nascimento070@gmail.com>
2025-07-07 23:07:43 -0300

    Add files via upload
    trabalho

---
+++ meuscript.js
@@ -0,0 +1,15 @@
+<script>
+    function exibirMensagem(){
+   alert("Olá, esses são meus principais interesses, resumidamente falando, sou uma pessoa diversa, com interesses interligados.");
+   }
+       
+</script>
+
+
+
+
+
+
+
+
+
